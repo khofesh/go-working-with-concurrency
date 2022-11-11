5f52d2465cf9258c7aed228272f64d9639c81c26
fahmi ahmad <fahmiahmad@tutanota.com>
2022-11-11 21:28:02 +0700

    fix race condition using mutex

--- race-mutexes-channels/example2/main.go
+++ race-mutexes-channels/example2/main.go
@@ -8,17 +8,23 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string) {
+func updateMessage(s string, m *sync.Mutex) {
 	defer wg.Done()
+
+	m.Lock()
 	msg = s
+
+	m.Unlock()
 }
 
 func main() {
 	msg = "hola mundo!"
 
+	var mutex sync.Mutex
+
 	wg.Add(2)
-	go updateMessage("hola universo!")
-	go updateMessage("hola cosmos!")
+	go updateMessage("hola universo!", &mutex)
+	go updateMessage("hola cosmos!", &mutex)
 	wg.Wait()
 
 	fmt.Println(msg)
